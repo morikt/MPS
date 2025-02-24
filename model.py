9e306334f087930fe0112eda1a5ac398a883b9b5
Maksim Matsnew <colddogger@gmail.com>
2025-02-24 05:45:08 +0300

    feat: create TASKS class

--- model.py
+++ model.py
@@ -1,2 +1,17 @@
+import datetime
 
-print("Hello world")
+
+class Tasks():
+    def __init__(self,task,
+                 date_added=None, date_completed=None,
+                 status=None, position="None"):
+        self.task = task
+        self.date_added = date_added if date_added is not None else datetime.datetime.now().isoformat()
+        self.date_completed = date_completed if date_completed is not None else None
+        self.status = status if status is not None else 1 #1-open 2-closed
+        self.position =  position if position is not None else None;
+
+    def __repr__(self) -> str:
+        return f"{self.task}, {self.date_added}, {self.date_completed}, {self.status}. {self.position}"
+
+    
