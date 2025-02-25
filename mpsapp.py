edea9e477c662f6c2664180ed81ab96b0d6c1eb0
Maksim Matsnew <colddogger@gmail.com>
2025-02-25 03:38:17 +0300

    feat: add all buttons

--- mpsapp.py
+++ mpsapp.py
@@ -12,19 +12,35 @@ class Window(Tk):
         self.title('Новое окно')
         self.geometry('250x250')
         self['bg'] = 'black'
+        self.window_add = Label(self, text='', bg="#480607", font=40, fg='white', width=100)
+        self.enter_add = Entry(self, width=50, font=40)
+        self.confirm_add = Button(self, text="Подтвердить", width=70, font=40, bg='#480607',fg='white',
+                                  command=self.button_clicked)
 
     def get_data(self):
         #enter
-        self.enter_add = Entry(self, width=50, font=40)
         self.enter_add.pack(pady=10)
-
         #button
-        self.confirm_add = Button(self, text="Подтвердить", width=70, font=40, bg='#480607',fg='white',
-                                  command=self.button_clicked)
         self.confirm_add.pack(pady=10)
 
     def button_clicked(self):
         add(self.enter_add.get())
+        show()
+        self.destroy()
+
+    def button_clicked1(self):
+        delete(int(self.enter_add.get()))
+        show()
+        self.destroy()
+
+    def button_clicked2(self):
+        a,b = map(str, self.enter_add.get().split())
+        update(a,b)
+        show()
+        self.destroy()
+
+    def button_clicked3(self):
+        complete(int(self.enter_add.get()))
         show()
         self.destroy()
 
@@ -44,8 +60,6 @@ frame.place(rely=0.1,relwidth=1,relheight=0.5)
 title = Label(frame, text='MPS', bg="#480607",font=40, fg='white',  width=350)
 title.pack()
 
-tasks = get_results()
-
 columns = ("#", "TASKS", "PROGRESS", "STARTING DATE", "ENDING DATE")
 tree = ttk.Treeview(frame, columns=columns, show="headings")
 tree.pack(fill=BOTH, expand=1)
@@ -62,27 +76,25 @@ tree.column("#3", stretch=YES, width=75)
 tree.column("#4", stretch=YES, width=100)
 tree.column("#5", stretch=YES, width=100)
 
-for idx, task in enumerate(tasks, start=1):
-    is_done_str = "✅" if task.status == 2 else "❌"
-    tree.insert("", END, values=(str(idx), task.task, is_done_str, task.date_added, task.date_completed))
-
 
 def show():
+    tree.delete(*tree.get_children())
     tasks = get_results()
-    is_done_str = "✅" if tasks[-1].status == 2 else "❌"
-    tree.insert("", END, values=(str(len(tasks)), tasks[-1].task, is_done_str, tasks[-1].date_added,
-                                 tasks[-1].date_completed))
+    for idx, task in enumerate(tasks, start=1):
+        is_done_str = "✅" if task.status == 2 else "❌"
+        tree.insert("", END, values=(str(idx), task.task, is_done_str, task.date_added, task.date_completed))
 
-
+show()
 
 btn_frame=Frame(root, bg='#480607')
 btn_frame.place(rely = 0.6,relheight=0.2,relwidth=1)
 
-
-def click_add_button():
+#click_button
+def click_add_button(text='Введите название'):
     window1 = Window()
-    window_add = Label(window1, text='Введите название: ', bg="#480607", font=40, fg='white', width=100)
-    window_add.pack()
+    window1.window_add['text'] = 'Введите название'
+    window1.window_add.pack()
+    window1.confirm_add['command'] = window1.button_clicked
     window1.get_data()
 
 def add(task: str):
@@ -90,40 +102,49 @@ def add(task: str):
     insert_task(one_task)
 
 
+def click_dell_button():
+    window1 = Window()
+    window1.window_add['text'] = 'Введите номер'
+    window1.window_add.pack()
+    window1.confirm_add['command'] = window1.button_clicked1
+    window1.get_data()
+
+def delete(position: int):
+    delete_task(position-1)
+
+
+def click_upd_button():
+    window1 = Window()
+    window1.window_add['text'] = 'Введите номер и имя'
+    window1.window_add.pack()
+    window1.confirm_add['command'] = window1.button_clicked2
+    window1.get_data()
+
+def update(position: str, task: str):
+    update_task(int(position)-1, task)
+
+
+def click_cmp_button():
+    window1 = Window()
+    window1.window_add['text'] = 'Введите номер'
+    window1.window_add.pack()
+    window1.confirm_add['command'] = window1.button_clicked3
+    window1.get_data()
+
+def complete(position: int):
+    complete_to_do(position-1)
+
 btn_add = Button(btn_frame, text="Add task",bg="black", fg="white", font=40, command=click_add_button)
-btn_add.place(rely=0.2, relheight=0.7,relwidth=0.25)
+btn_add.place(rely=0.2, relx=0.08, relheight=0.7,relwidth=0.2)
 
+btn_dell = Button(btn_frame, text="Delete",bg="black", fg="white", font=40, command=click_dell_button)
+btn_dell.place(rely=0.2, relx=0.3, relheight=0.7,relwidth=0.2)
 
-#
-# def delete(position: int):
-#     typer.echo(f"deleting {position}")
-#     delete_task(position-1)
-#     show()
-#
-# def update(position: int, task: str):
-#     typer.echo(f"updating {position}")
-#     update_task(position-1, task)
-#     show()
-#
-# def complete(position: int):
-#     typer.echo(f"complete {position}")
-#     complete_to_do(position-1)
-#     show()
-#
-# def show():
-#     tasks = get_results()
-#     console.print("[bold red]DEADLINES[/bold red]")
-#
-#     table = Table(show_header=True, header_style="bold red")
-#     table.add_column("#", style="dim", width=6, justify="center")
-#     table.add_column("TASKS", min_width = 20, justify="center")
-#     table.add_column("PROGRESS", min_width = 12, justify="center")
-#     table.add_column("STATING DATE", min_width=25, justify="center")
-#     table.add_column("ENDING DATE", min_width=25, justify="center")
-#
-#     for idx,task in enumerate(tasks, start=1):
-#         is_done_str = "✅" if task.status==2 else "❌"
-#         table.add_row(str(idx), task.task, is_done_str, task.date_added, task.date_completed)
-#     console.print(table)
+btn_upd = Button(btn_frame, text="Update",bg="black", fg="white", font=40, command=click_upd_button)
+btn_upd.place(rely=0.2,relx=0.52, relheight=0.7,relwidth=0.2)
+
+btn_cpl = Button(btn_frame, text="Complete",bg="black", fg="white", font=40, command=click_cmp_button)
+btn_cpl.place(rely=0.2,relx=0.74, relheight=0.7,relwidth=0.2)
+
 
 root.mainloop()
